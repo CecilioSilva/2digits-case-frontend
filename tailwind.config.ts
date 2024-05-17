da2919621edfedbf036e318ad3f2f71fd82c1421
CecilioSilva <ceciliostar@gmail.com>
2024-05-17 10:09:30 +0200

    feat(tailwind): updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import { withTV } from 'tailwind-variants/transformer';
+import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
-const config = withTV({
+const config: Config = withTV({
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -11,6 +13,17 @@ const config = withTV({
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        headerGradient: 'radial-gradient(circle at 70% 60%, #020365 0%, #01041F 99.95%);',
+      },
+      colors: {
+        light: '#FFFFFF',
+        dark: '#000000',
+        offwhite: '#EFEFF8',
+        primary: '#0E1527',
+      },
+      fontFamily: {
+        barlow: ['var(--font-barlow)', ...fontFamily.serif],
+        fira: ['var(--font-fira-sans)', ...fontFamily.sans],
       },
     },
   },
